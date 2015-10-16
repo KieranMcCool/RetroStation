69587203ce6773d23a878c23acf14d63939421f7
Kieran McCool <kiwi332@users.noreply.github.com>
2015-10-16 19:18:52 +0100

    Directory Structure Changed

--- RetroStationUpdater/Program.cs
+++ RetroStationUpdater/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RetroStationUpdater
 {
@@ -10,7 +11,7 @@ namespace RetroStationUpdater
     {
         static void Main(string[] args)
         {
-
+            if()
         }
     }
 }
