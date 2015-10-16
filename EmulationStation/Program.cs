414ce421e5f52af2783de9a9820559a24b2b1954
Kieran McCool <kiwi332@users.noreply.github.com>
2015-10-16 21:51:22 +0100

    framework for auto updater

--- EmulationStation/Program.cs
+++ EmulationStation/Program.cs
@@ -142,25 +142,34 @@ namespace EmulationStation
 
         private static bool updateCheck()
         {
+            var wc = new System.Net.WebClient();
             string version;
             string onlineVersion;
             bool debug = false;
-#if DEBUG
-            debug = true;
-#endif
+//#if DEBUG
+//            debug = true;
+//#endif
             // We don't want to update every time we debug the program as this will revert.
             if (debug)
                 return true;
-
+            string versionInfoURL = @"https://raw.githubusercontent.com/KieranMcCool/RetroStation/master/Versions/RetroStationLatest/Resources/BuildDate.txt";
+            string versionArchiveURL = @"https://github.com/KieranMcCool/RetroStation/blob/master/Versions/RetroStationLatest.zip?raw=true";
             using (var sr = new StreamReader(Environment.CurrentDirectory + @"\Resources\BuildDate.txt"))
                 version = sr.ReadToEnd();
-            using (var wc = new System.Net.WebClient())
-                onlineVersion = 
-                    wc.DownloadString(@"https://raw.githubusercontent.com/KieranMcCool/RetroStation/master/Versions/RetroStationLatest/Resources/BuildDate.txt");
+                onlineVersion = wc.DownloadString(versionInfoURL);
             if (version == onlineVersion)
                 return true;
             else
-                throw new NotImplementedException();
+            {
+                wc.DownloadFile(versionArchiveURL, "DL.zip");
+                System.Diagnostics.Process p = new System.Diagnostics.Process();
+                p.StartInfo = new System.Diagnostics.ProcessStartInfo()
+                {
+                    FileName = "RetroStationUpdater.exe"
+                };
+                p.Start();
+                return false;
+            }
         }
 
         private static bool platformsCheck()
--- RetroStationUpdater/Program.cs
+++ RetroStationUpdater/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 
 
 namespace RetroStationUpdater
@@ -12,8 +14,64 @@ namespace RetroStationUpdater
     {
         static void Main(string[] args)
         {
-            if (Directory.Exists(Environment.CurrentDirectory + @"\DL")) ;
-
+            if (File.Exists(Environment.CurrentDirectory + @"\DL.zip"))
+            {
+                ExtractZipFile(Environment.CurrentDirectory + @"\DL.zip", "\tmp");
+            }
         }
+
+        private static void replaceOldFiles()
+        {
+            foreach (string s in Directory.GetFiles("\tmp"))
+            {
+                FileInfo fi = new FileInfo(s);
+                File.Copy(s, fi.Name, true);
+            } 
+        }
+
+        private void cleanUp()
+        {
+            File.Delete("DL.zip");
+            Directory.Delete("\tmp");
+        }
+
+        private static void ExtractZipFile(string archiveFilenameIn, string outFolder)
+        {
+            ZipFile zf = null;
+            try
+            {
+                FileStream fs = File.OpenRead(archiveFilenameIn);
+                zf = new ZipFile(fs);
+                foreach (ZipEntry zipEntry in zf)
+                {
+                    if (!zipEntry.IsFile)
+                    {
+                        continue;
+                    }
+                    String entryFileName = zipEntry.Name;
+
+                    byte[] buffer = new byte[4096];
+                    Stream zipStream = zf.GetInputStream(zipEntry);
+                    
+                    String fullZipToPath = Path.Combine(outFolder, entryFileName);
+                    string directoryName = Path.GetDirectoryName(fullZipToPath);
+                    if (directoryName.Length > 0)
+                        Directory.CreateDirectory(directoryName);
+                    using (FileStream streamWriter = File.Create(fullZipToPath))
+                    {
+                        StreamUtils.Copy(zipStream, streamWriter, buffer);
+                    }
+                }
+            }
+            finally
+            {
+                if (zf != null)
+                {
+                    zf.IsStreamOwner = true; // Makes close also shut the underlying stream
+                    zf.Close(); // Ensure we release resources
+                }
+            }
+        }
+
     }
 }
